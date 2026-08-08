64023f171494d09ffc73954b2c06ff22782416c5
SravanthiLC <chamarthylsravanthi@gmail.com>
2026-08-08 18:56:42 +0000

    Implemented Random Forest Regressor

--- src/train.py
+++ src/train.py
@@ -4,6 +4,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
 
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+
 # load the cleaned dataset
 df = load_data()
 
@@ -17,6 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     random_state = 42
 )
 def linear_regression():
+    print("\nLINEAR REGRESSION")
     # create the preprocessing pipeline
     lr_preprocessor = create_preprocessor(X_train)
 
@@ -45,4 +49,72 @@ def linear_regression():
     print(f"Root Mean Squared Error : {lr_rmse:.2f}")
     print(f"R2 Score : {lr_r2:.2f}")
 
+    # Linear Regression Results:
+    # Mean Absolute Error : 1715.02
+    # Root Mean Squared Error : 3743.75
+    # R2 Score : 0.59
+
+
+def random_forest():
+    print("\nRANDOM FOREST REGRESSOR")
+    # create the preprocessing pipeline
+    rf_preprocessor = create_preprocessor(X_train)
+
+    # build the pipeline
+    rf_pipeline = Pipeline(
+        steps = [
+            ("preprocessor", rf_preprocessor),
+            ("regressor", RandomForestRegressor(random_state = 42))
+        ]
+    )
+
+    # using GridSearchCV to train multiple models by using different combinations of parameters
+    rf_param_grid = {
+        "regressor__n_estimators" : [100, 200, 300],
+        "regressor__max_depth" : [10, 15, 20],
+        "regressor__min_samples_split" : [2, 5, 10],
+        "regressor__min_samples_leaf" : [1, 2],
+    }
+
+    # perform grid search
+    rf_grid_search = GridSearchCV(
+        estimator = rf_pipeline,
+        param_grid = rf_param_grid,
+        scoring = "r2",
+        cv = 3,
+        n_jobs = -1,
+        verbose = 1
+    )
+
+    # train all parameter combinations
+    rf_grid_search.fit(X_train, y_train)
+
+    # best model
+    best_rf_model = rf_grid_search.best_estimator_
+
+    print("\nBest Random Forest Parameters: ")
+    print(rf_grid_search.best_params_)
+
+    # make predictions
+    y_pred = best_rf_model.predict(X_test)
+
+    # evaluate the model
+
+    rf_mae = mean_absolute_error(y_test, y_pred)
+    rf_rmse = root_mean_squared_error(y_test, y_pred)
+    rf_r2 = r2_score(y_test, y_pred)
+
+    # display results
+
+    print("\nRandom Forest Results:")
+    print(f"Mean Absolute Error : {rf_mae:.2f}")
+    print(f"Root Mean Squared Error : {rf_rmse:.2f}")
+    print(f"R2 Score : {rf_r2:.2f}")
+
+    # Random Forest Results:
+    # Mean Absolute Error : 1404.39
+    # Root Mean Squared Error : 3404.88
+    # R2 Score : 0.66
+
 linear_regression()
+random_forest()
